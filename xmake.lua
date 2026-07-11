47d20afb473173a269c2897fe54063e748ea507e
MacroModel <liyn_xlfd@163.com>
2026-07-11 13:06:55 +0800

    Update project version to 2.0.4 and enhance documentation
    - Incremented project version from 2.0.3 to 2.0.4 in doxyfile and README.md.
    - Updated README.md to reflect the new version in badges and release notes.
    - Adjusted xmake.lua and related scripts to align with the new versioning.
    - Enhanced documentation for clarity and consistency across various components.

--- xmake.lua
+++ xmake.lua
@@ -4,10 +4,10 @@ set_project("uwvm")
 set_policy("check.auto_ignore_flags", false)
 
 -- Version
-set_version("2.0.3")
+set_version("2.0.4")
 add_defines("UWVM_VERSION_X=2")
 add_defines("UWVM_VERSION_Y=0")
-add_defines("UWVM_VERSION_Z=3")
+add_defines("UWVM_VERSION_Z=4")
 add_defines("UWVM_VERSION_S=0")
 add_defines("UWVM_VERSION_DEV")
 
