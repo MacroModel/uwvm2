c26269990cd3e66a01dec8265bf6766e91cc6953
MacroModel <liyn_xlfd@163.com>
2026-09-04 11:35:03 +0800

    [validation] Route layered feature policy through code validation

--- src/uwvm2/uwvm/runtime/validator/validate.h
+++ src/uwvm2/uwvm/runtime/validator/validate.h
@@ -46,6 +46,7 @@
 # include <uwvm2/validation/impl.h>
 # include <uwvm2/validation/standard/wasm1/impl.h>
 # include <uwvm2/validation/standard/wasm1p1/impl.h>
+# include <uwvm2/validation/standard/wasm2/impl.h>
 # include <uwvm2/object/impl.h>
 # include <uwvm2/uwvm/io/impl.h>
 # include <uwvm2/uwvm/utils/ansies/impl.h>
@@ -84,13 +85,12 @@ UWVM_MODULE_EXPORT namespace uwvm2::uwvm::runtime::validator
             try
 #endif
             {
-                ::uwvm2::validation::standard::wasm1p1::validate_code(::uwvm2::validation::standard::wasm1p1::wasm1p1_code_version{},
-                                                                      module_storage,
-                                                                      import_func_count + local_idx,
-                                                                      code_begin_ptr,
-                                                                      code_end_ptr,
-                                                                      v_err,
-                                                                      fs_para);
+                ::uwvm2::validation::standard::wasm2::validate_code_with_runtime_policy(module_storage,
+                                                                                        import_func_count + local_idx,
+                                                                                        code_begin_ptr,
+                                                                                        code_end_ptr,
+                                                                                        v_err,
+                                                                                        fs_para);
             }
 #ifdef UWVM_CPP_EXCEPTIONS
             catch(::fast_io::error)
